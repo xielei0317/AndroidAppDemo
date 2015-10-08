a0f25b27bea2f4160c5c040b9937868d06eb7b57
lunpeng_xie <122378805@qq.com>
2015-10-08 16:50:06 +0800

    no message

--- app/src/androidTest/java/com/example/gigold/gigoldandroidapp/TestMyApp.java
+++ app/src/androidTest/java/com/example/gigold/gigoldandroidapp/TestMyApp.java
@@ -12,66 +12,58 @@ import junit.framework.Assert;
  * Created by xie_lp on 15/10/8.
  */
 public class TestMyApp extends InstrumentationTestCase {
-    MainActivity mActivity = null;
+    private MainActivity sample = null;
     private Button button = null;
-
     private TextView text = null;
 
-    public void testSample() throws Throwable {
-        Assert.assertTrue(1 + 1 == 2); // 测试一个错误的结果
-    }
-
+    /*
+     * 初始设置
+     * @see junit.framework.TestCase#setUp()
+     */
     @Override
-    protected void tearDown() throws Exception {
-        // TODO Auto-generated method stub
-        mActivity.finish();
-
-        super.tearDown();
-    }
-
-    @Override
-    protected void setUp() throws Exception {
-        // TODO Auto-generated method stub
-        super.setUp();
+    protected void setUp()  {
+        try {
+            super.setUp();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         Intent intent = new Intent();
-
         intent.setClassName("com.example.gigold.gigoldandroidapp", MainActivity.class.getName());
-
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-        mActivity = (MainActivity) getInstrumentation().startActivitySync(
-                intent);
-
-        text = (TextView) mActivity.findViewById(R.id.text1);
-
-        button = (Button) mActivity.findViewById(R.id.id_btn_fun);
-
+        sample = (MainActivity) getInstrumentation().startActivitySync(intent);
+        text = (TextView) sample.findViewById(R.id.text1);
+        button = (Button) sample.findViewById(R.id.button1);
     }
 
     /*
-     *
+     * 垃圾清理与资源回收
+     * @see android.test.InstrumentationTestCase#tearDown()
+     */
+    @Override
+    protected void tearDown()  {
+        sample.finish();
+        try {
+            super.tearDown();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
      * 活动功能测试
      */
-
     public void testActivity() throws Exception {
-        // 测试键壮性，连续运行某项功能100次，点击Button 100次
-        for (int i = 0; i < 100; ++i) {
-            getInstrumentation().runOnMainSync(new PerformClick(button));
-            SystemClock.sleep(500); // 中间间隔 0.5秒
-        }
-        assertEquals("Android InstrumentationTestCase", text.getText().toString()); //检查运行后的输出结果
-
+//        Log.v("testActivity", "test the Activity");
+        SystemClock.sleep(1500);
+        getInstrumentation().runOnMainSync(new PerformClick(button));
+        SystemClock.sleep(3000);
+        assertEquals("Hello Android", text.getText().toString());
     }
-
     /*
-     *
      * 模拟按钮点击的接口
      */
-
     private class PerformClick implements Runnable {
-
         Button btn;
-
         public PerformClick(Button button) {
             btn = button;
         }
@@ -79,7 +71,16 @@ public class TestMyApp extends InstrumentationTestCase {
         public void run() {
             btn.performClick();
         }
+    }
 
+    /*
+     * 测试类中的方法
+     */
+    public void testAdd() throws Exception{
+        String tag = "testAdd";
+//        Log.v(tag, "test the method");
+        int test = sample.add(1, 1);
+        assertEquals(2, test);
     }
 }
 
--- app/src/main/java/com/example/gigold/gigoldandroidapp/MainActivity.java
+++ app/src/main/java/com/example/gigold/gigoldandroidapp/MainActivity.java
@@ -5,54 +5,28 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView tv = null;
+    private TextView myText = null;
+    private Button button = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv = (TextView) this.findViewById(R.id.text1);
-        this.findViewById(R.id.id_btn_fun).setOnClickListener(mOnClickListener);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    View.OnClickListener mOnClickListener = new View.OnClickListener() {
-
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()) {
-                case R.id.id_btn_fun:
-                    fun();
-                    break;
-                default:
-
+        myText = (TextView) findViewById(R.id.text1);
+        button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                myText.setText("Hello Android");
             }
-        }
-    };
+        });
+    }
 
-    private void fun(){
-        tv.setText(android.os.Build.MODEL + android.os.Build.getRadioVersion());
+    public int add(int i, int j) {
+        return (i + j);
     }
 }
